ae70fd80d1721d715a9c39cda7cf835b1ccad5a6
Michael Aherne <micaherne@blueyonder.co.uk>
2016-09-07 23:07:05 +0100

    Cleanup

--- app.php
+++ app.php
@@ -4,6 +4,8 @@ require_once 'vendor/autoload.php';
 
 $moodleroot = realpath(__DIR__ . '/moodle');
 
+// This currently requires a whitelist.json file, which is only
+// generated as a cache file by EntryPointIterator at the moment.
 $json = file_get_contents(__DIR__ . '/whitelist.json');
 $whitelist = json_decode($json);
 
